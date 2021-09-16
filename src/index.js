2c2a00fd58a9a1849971b039ba4799b3987308ff
Vedansh Wani <me.vedanshwani@gmail.com>
2021-09-16 13:44:21 +0530

    eslint config

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import "./index.css";
 import numeral from "numeral";
 
 const bDay = numeral(22071998).format("dd/mm/yyyy");
-console.log(bDay);
+
+console.log(bDay); // eslint-disable-line no-console
